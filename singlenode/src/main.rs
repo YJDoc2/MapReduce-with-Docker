73f15080a3012c953aca66a025e4af4b39155d22
Yashodhan Joshi <yjdoc2@gmail.com>
2021-11-17 19:24:25 +0530

    Add matrix single node

--- singlenode/src/main.rs
+++ singlenode/src/main.rs
@@ -6,8 +6,56 @@ use std::time::Instant;
 fn get_input_file() -> String {
     std::env::var("INPUT").unwrap()
 }
-fn main() {
-    let time = Instant::now();
+
+#[allow(dead_code)]
+fn generate_rand_matrix(count: usize, threshold: f32, range: f32, mr_name: &str, sn_name: &str) {
+    use rand::prelude::*;
+    let mut a: Vec<Vec<f32>> = Vec::with_capacity(count);
+    let mut b: Vec<Vec<f32>> = Vec::with_capacity(count);
+    let mut rng = thread_rng();
+    let mut f = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(mr_name)
+        .unwrap();
+    for i in 0..count {
+        let mut temp = Vec::with_capacity(count);
+        for j in 0..count {
+            let decider: f32 = rng.gen();
+            if decider < threshold {
+                let rand_num: f32 = rng.gen_range(0.0..range);
+                writeln!(f, "A {} {} {}", i, j, rand_num).unwrap();
+                temp.push(rand_num);
+            } else {
+                temp.push(0.0);
+            }
+        }
+        a.push(temp);
+    }
+    for i in 0..count {
+        let mut temp = Vec::with_capacity(count);
+        for j in 0..count {
+            let decider: f32 = rng.gen();
+            if decider < threshold {
+                let rand_num: f32 = rng.gen_range(0.0..range);
+                writeln!(f, "B {} {} {}", i, j, rand_num).unwrap();
+                temp.push(rand_num);
+            } else {
+                temp.push(0.0);
+            }
+        }
+        b.push(temp);
+    }
+    let mut f = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(sn_name)
+        .unwrap();
+    writeln!(f, "{}", serde_json::to_string(&[a, b]).unwrap()).unwrap();
+}
+
+#[allow(dead_code)]
+fn wordcount_main() {
     let mut fpath = PathBuf::from(get_input_file());
     println!("Starting...");
     let ip = std::fs::read_to_string(fpath.clone()).unwrap();
@@ -20,13 +68,48 @@ fn main() {
         *ctr += 1;
     }
     fpath.pop();
+    let mut t = String::new();
+    t = hm.iter().fold(t, |t, (k, v)| t + &format!("{} {}\n", k, v));
     let mut f = std::fs::OpenOptions::new()
         .write(true)
         .create(true)
         .open(fpath.join("singlenode.txt"))
         .unwrap();
-    let mut t = String::new();
-    t = hm.iter().fold(t, |t, (k, v)| t + &format!("{} {}\n", k, v));
     writeln!(f, "{}", t).unwrap();
+}
+
+#[allow(dead_code)]
+fn matrix_main() {
+    let mut fpath = PathBuf::from(get_input_file());
+    println!("Starting...");
+    let ip = std::fs::read_to_string(fpath.clone()).unwrap();
+    let [a, b]: [Vec<Vec<f32>>; 2] = serde_json::from_str(&ip).unwrap();
+    fpath.pop();
+    let mut ret = Vec::with_capacity(a.len() * a.len());
+    // we know that these both have equal size, as we have generated he data
+    for i in 0..a.len() {
+        println!("iter {}/500", i + 1);
+        for j in 0..a.len() {
+            let mut sum = 0.0;
+            for k in 0..a.len() {
+                sum += a[i][k] * b[k][j];
+            }
+            ret.push(sum);
+        }
+    }
+    let mut f = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(fpath.join("singlenode.txt"))
+        .unwrap();
+    let t: usize = a.len();
+    for (i, v) in ret.iter().enumerate() {
+        writeln!(f, "{} {} {}", i / t, i % t, v).unwrap();
+    }
+}
+
+fn main() {
+    let time = Instant::now();
+    matrix_main();
     println!("{:?}", time.elapsed());
 }
